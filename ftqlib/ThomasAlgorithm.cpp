de2892e0e09b7dae59c66dba9535dfe975757480
Artemy Sazonov <90921891+AASazonov@users.noreply.github.com>
2023-12-15 12:46:19 +0300

    `std::size_t` hotfix

--- ftqlib/ThomasAlgorithm.cpp
+++ ftqlib/ThomasAlgorithm.cpp
@@ -9,7 +9,7 @@
 void thomas_algorithm(std::vector<double>& v, std::vector<double>& a,
                       std::vector<double>& b, std::vector<double>& c,
                       std::vector<double>& f) {
-  size_t n = a.size() - 1;
+  std::size_t n = a.size() - 1;
 
   std::vector<double> alpha;
   alpha.resize(n + 1);
@@ -20,14 +20,14 @@ void thomas_algorithm(std::vector<double>& v, std::vector<double>& a,
   alpha[2] = b[1] / c[1];
   beta[2] = f[1] / c[1];
 
-  for (size_t j = 2; j <= n - 1; j++) {
+  for (std::size_t j = 2; j <= n - 1; j++) {
     alpha[j + 1] = b[j] / (c[j] - a[j] * alpha[j]);
     beta[j + 1] = (f[j] + a[j] * beta[j]) / (c[j] - a[j] * alpha[j]);
   }
 
   v[n] = (f[n] + a[n] * beta[n]) / (c[n] - a[n] * alpha[n]);
 
-  for (size_t j = n - 1; j >= 1; j--) {
+  for (std::size_t j = n - 1; j >= 1; j--) {
     v[j] = beta[j + 1] + alpha[j + 1] * v[j + 1];
   }
 }
